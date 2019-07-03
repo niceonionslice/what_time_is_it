923fca770341cd68393de840111e2825b3df1c9b
niceonionslice <niceonionslice@gmail.com>
2019-07-03 23:37:27 +0900

    update: リアルタイムで更新する時間を表示できるようにしました。

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1;
-const day = date.getDate();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-// const milliseconds = date.getMilliseconds();
-const label = `今日は${year}/${month}/${day} ${hours}:${minutes}:${seconds}です。`;
-
-
 window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector(".main--title").innerHTML = label;  
-})
+  setInterval(rooplabel, 1000);
+});
+
+const rooplabel = () => {
+  document.querySelector(".main--title").innerHTML = dateNow();
+  console.log(dateNow());
+}
+
+const dateNow = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  // const milliseconds = date.getMilliseconds();
+  const label = `今日は${year}/${month}/${day} ${hours}:${minutes}:${seconds}です。`;
+  return label;
+}
